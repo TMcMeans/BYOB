494fdc768f33209c1a6b9928e37b8a5140714c10
Ashton Bailey <ashtonkbailey@gmail.com>
2019-01-29 15:55:02 -0700

    Write test for happy path of client routes

--- server.js
+++ server.js
@@ -54,3 +54,5 @@ app.patch('/api/v1/festivals/:festivalID', (request, response) => {
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}`)
 });
+
+module.exports = app;
---
+++ test/routes.spec.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server.js');
+const states = require('../data/states.js');
+const festivals = require('../data/festivals.js');
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+  // front end
+
+  // happy path
+  it('should return the homepage with text', done => {
+    chai.request(server)
+    .get('/')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.html;
+      response.res.text.should.equal('US Music Festivals API');
+      done();
+    })
+
+
+  })
+});
+
+describe('API Routes', () => {
+  before(done => {
+   // Run migrations and seeds for test database
+   done();
+  });
+
+  beforeEach((done) => {
+   // Would normally run your seed(s), which includes clearing all records
+   // from each of the tables
+   server.locals.states = states;
+   server.locals.festivals = festivals;
+   done();
+  });
+})
