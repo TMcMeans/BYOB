8b5a6a76dd196f832ec51d965788de4239a431d0
Ashton Bailey <ashtonkbailey@gmail.com>
2019-01-29 14:55:36 -0700

    Write get, post, patch, and delete endpoints

--- data/festivals.js
+++ data/festivals.js
@@ -1,4 +1,4 @@
-const festivals = {
+ const festivals = {
   'Arizona': [
     { festival_name: 'Arizona Roots', start_end_dates: '2/9/19-2/10/19', city: 'Chandler', image: 'https://1b7ta73fjmj23201tc3suvsi-wpengine.netdna-ssl.com/wp-content/uploads/2016/10/California-Roots-Hero-Pic-600x400.jpg' },
     { festival_name: 'Crush Arizona', start_end_dates: '2/15/19-2/16/19', city: 'Chandler', image: 'https://1b7ta73fjmj23201tc3suvsi-wpengine.netdna-ssl.com/wp-content/uploads/2018/12/Crush-Arizona-600x400.jpg' },
--- server.js
+++ server.js
@@ -11,8 +11,46 @@ app.get('/', (request, response) => {
   response.send(`${app.locals.title}`)
 });
 
+app.get('/api/v1/states', (request, response) => {
+  // get all states
+});
 
+app.get('/api/v1/festivals', (request, response) => {
+  // get all festivals
+});
+
+app.get('/api/v1/states/:stateID/festivals', (request, response) => {
+  // get all festivals in a given state
+});
+
+app.get('/api/v1/festivals/:festivalID', (request, response) => {
+  // get a festival by id
+});
+
+app.post('/api/v1/states', (request, response) => {
+  // add to states
+});
+
+app.post('/api/v1/festivals', (request, response) => {
+  // add to festivals
+});
+
+app.delete('/api/v1/states/:stateID', (request, response) => {
+  // delete a state by id
+});
+
+app.delete('/api/v1/festivals/:festivalID', (request, response) => {
+  // delete a festival by id
+});
+
+app.patch('/api/v1/states/:stateID', (request, response) => {
+  // update a state by id
+});
+
+app.patch('/api/v1/festivals/:festivalID', (request, response) => {
+  // update a festival by id
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}`)
-})
+});
