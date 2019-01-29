f178d630666c154142df406c3de1ba20adbe1266
Ashton Bailey <ashtonkbailey@gmail.com>
2019-01-29 14:03:26 -0700

    Set up the server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'US Music Festivals API';
+app.locals.states = [];
+app.locals.festivals = {};
+
+app.get('/', (request, response) => {
+  response.send(`${app.locals.title}`)
+});
+
+
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}`)
+})
