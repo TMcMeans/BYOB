f6c928659e7ccc81faab4b321ae54239d9fb2df6
Ashton Bailey <ashtonkbailey@gmail.com>
2019-02-03 19:41:20 -0700

    Deploy to Heroku

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const environment = process.env.NODE_ENV || 'development';
-
 const config = require('./knexfile')[environment];
 const database = require('knex')(config);
 const express = require('express');
